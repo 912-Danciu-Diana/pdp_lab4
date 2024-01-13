460fa036917bd73a08c69c07c67baa395fa44587
912-Danciu-Diana <diana.ioana.danciu@gmail.com>
2024-01-13 14:24:44 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿﻿using System.Diagnostics;
+using Lab4.Implementation;
+
+namespace Lab4;
+
+public class Program
+{
+    static void Main()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        var hosts = new List<string>
+        {
+            "www.ubbcluj.ro",
+            //"www.google.com",
+            //"www.youtube.com",
+           
+        };
+        
+        Console.WriteLine("*******************************DIRECT CALLBACK**************************************");
+        DirectCallback.Run(hosts);
+        
+        Console.WriteLine("***********************************TASKS********************************************");
+        TaskMechanism.Run(hosts);
+        
+        
+        Console.WriteLine("*******************************ASYNC/AWAIT TASKS************************************");
+        AsyncTaskMechanism.Run(hosts);
+    
+    }
+}
